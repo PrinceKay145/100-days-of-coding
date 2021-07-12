3a39365c54d6b09ad9e3fbf055d04ffc9ed392b8
Adebayo Ridwan <73721302+PrinceKay145@users.noreply.github.com>
2021-07-12 17:09:36 +0300

    Add files via upload

--- Day13.py
+++ Day13.py
@@ -1,4 +1,4 @@
-menu = {
+MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
@@ -24,49 +24,75 @@ menu = {
     }
 }
 
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
-def report():
-    water = resources['water']
-    milk = resources['milk']
-    coffee = resources['coffee']
-    money = 30
-    return f"Water: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g \nMoney: ${money}"
-# TODO 1. The prompt have to show everytime a drink is sold successfully
-start= input("What would you like? (espresso/latte/cappuccino): ").lower()
-coffee_fee = 0
-change = 0
-def get_a_coffee(start, menu, resources):
-    if start == "espresso":
-        print("Please insert coins.")
-        take_quarters= int(input("How many quarters?: "))
-        take_dimes= int(input("How many dimes?: "))
-        take_nickles = int(input("How many nickles?: "))
-        take_pennies = int(input("How many pennies?: "))
-        quarters = take_quarters * 0.25
-        dimes = take_dimes * 0.1
-        nickles = take_nickles * 0.05
-        pennies = take_pennies * 0.01
-        coffee_fee = pennies+dimes+nickles+quarters
-        change = coffee_fee - [start]['cost']
 
-        if menu[start]["ingredients"]["water"] < resources["water"]:
-            if menu[start]["ingredients"]["coffee"] < resources["coffee"]:
-                print (f"Here is your ${change} change")
-                print(f"Here is your {start}. Enjoy!")
-            else:
-                print(f"Sorry there is no enough coffee")
-        else:
-            print("Sorry there is no enough water")
-    elif start == "latte":
-        print("B")
-    elif start == "cappuccino":
-        print("C")
-    elif start == "off":
-        return
-    elif start == "report":
-        print(report())
-get_a_coffee(start, menu, resources)
+
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"​Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️. Enjoy!")
+
+
+is_on = True
+
+while is_on:
+    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
+
+
+
+
+
+
+
+
